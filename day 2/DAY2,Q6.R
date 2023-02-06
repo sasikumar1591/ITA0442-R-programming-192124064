9f36da9ad3fa5912eb1a39cfa24cef700b7d7398
sasikumar1591 <113574444+sasikumar1591@users.noreply.github.com>
2023-02-06 10:53:28 +0530

    Create DAY2,Q6.R

---
+++ day 2/DAY2,Q6.R
@@ -0,0 +1,11 @@
+exam_data = data.frame(
+  name = c('Anastaia', 'Dima', 'katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'kevin', 'Joans'),
+  soure = c(12.5, 9, 16.5, 12, 9, 20, 14.5, 13.5, 8, 19),
+  attepts = c(1, 3, 2, 3, 2, 3, 1, 1, 2, 1),
+  qualify = c('Yes', 'No', 'Yes', 'No', 'No', 'Yes', 'Yes', 'NO', 'NO', 'Yes')
+)
+print("Original dataframe:")
+print(exam_data)
+print("New data ftame after adding the 'country' column : ")
+exam_data$country = c('USA', 'USA', 'USA', 'USA', 'UK', 'USA', 'USA', 'INDIA', 'USA', 'USA')
+print(exam_data)
